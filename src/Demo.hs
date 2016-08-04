b8365b5cc7342766fdbc504606ce96d3fda701cc
Nathan Bloomfield <nbloomf@gmail.com>
2016-08-04 10:05:47 -0500

    generate demos concurrently

--- src/Demo.hs
+++ src/Demo.hs
@@ -2,21 +2,34 @@ module Demo (demo) where
 
 import Yentl
 import System.Directory
+import Control.Concurrent
+import Control.Monad (forever)
+import Control.Concurrent.Chan
+import Control.Concurrent.Async
+
+
+writer :: Chan String -> IO ()
+writer chan = do
+  forever $ do
+    threadDelay 500
+    gossip <- readChan chan
+    putStrLn gossip
 
 
 demoImg :: (Show t, ToCartesian t) =>
-  Fig t a -> Format -> ViewOpts -> FilePath -> String -> String -> IO ()
-demoImg fig format opts path name desc = do
-  putStrLn $ "building " ++ path ++ name
+  Chan String -> Fig t a -> Format -> ViewOpts -> FilePath -> String -> String -> IO ()
+demoImg chan fig format opts path name desc = do
+  writeChan chan $ "building " ++ path ++ name
   createDirectoryIfMissing True path
   xs <- viewIO fig
   writeFile (path ++ name) $ writeFormat format opts xs
   writeFile (path ++ "readme.txt") desc
+  writeChan chan $ "built " ++ path ++ name
 
 demoAni :: (Show t, ToCartesian t) =>
-  (Rational -> Fig t a) -> ViewOpts -> AnimateOpts -> FilePath -> String -> String -> IO ()
-demoAni fig vOpts aOpts path name desc = do
-  putStrLn $ "building " ++ path ++ name
+  Chan String -> (Rational -> Fig t a) -> ViewOpts -> AnimateOpts -> FilePath -> String -> String -> IO ()
+demoAni chan fig vOpts aOpts path name desc = do
+  writeChan chan $ "building " ++ path ++ name
   createDirectoryIfMissing True path
   animate fig aOpts vOpts path name
   writeFile (path ++ "readme.txt") desc
@@ -28,100 +41,148 @@ demoAni fig vOpts aOpts path name desc = do
         "\t  done\n" ++
         "\tconvert -dispose previous " ++ name ++ "*.png " ++ name ++ ".gif"
   writeFile (path ++ "makefile") make
+  writeChan chan $ "built " ++ path ++ name
 
 
 demo :: IO ()
-demo = sequence_
-  [ let
-      fig :: Fig CartesianPlane ()
-      fig = do
-        p <- coords (0,0) >>= pen plain
-        q <- coords (1,0) >>= pen plain
-        r <- coords (0,1) >>= pen plain
-        segment p q >>= pen plain
-        segment q r >>= pen plain
-        segment r p >>= pen plain
-        return ()
-
-      vOpts = bestFit 1 defaultView
-    in
-      demoImg fig EPS vOpts "demo/001/" "001.eps"
-        "Drawing of a triangle in the cartesian plane.\n"
+demo = do
+  chan <- newChan
+  forkIO $ writer chan
+  mapConcurrently ($ chan)
+    [ demo001
+    , demo002
+    , demo003
+    , demo004
+    , demo005
+    , demo006
+    ]
+  return ()
 
 
-  , let
-      fig :: Rational -> Fig CartesianPlane ()
-      fig t = do
-        p <- coords $ path (CirclePath (0,0) 8 0 1) t
-        pen plain p
-        q <- coords (0,0) >>= pen plain
-        segment p q >>= pen plain
-        return ()
+demo001 :: Chan String -> IO ()
+demo001 chan = do
+  let
+    fig :: Fig CartesianPlane ()
+    fig = do
+      p <- coords (0,0) >>= pen plain
+      q <- coords (1,0) >>= pen plain
+      r <- coords (0,1) >>= pen plain
+      segment p q >>= pen plain
+      segment q r >>= pen plain
+      segment r p >>= pen plain
+      return ()
 
-      vOpts = defaultView
-      aOpts = defaultAnimate
-    in
-      demoAni fig vOpts aOpts "demo/002/" "002"
-        "Animation of a rotating segment in the cartesian plane.\n"
-
-
-  , let
-      fig :: Fig PoincarePlane ()
-      fig = do
-        p <- coords (1,1) >>= pen plain
-        q <- coords (-2,2) >>= pen plain
-        r <- coords (0,3) >>= pen plain
-        segment p q >>= pen plain
-        segment q r >>= pen plain
-        segment r p >>= pen plain
-        return ()
-
-      vOpts = fixed ((-3,-1),(3,4)) defaultView
-    in
-      demoImg fig EPS vOpts "demo/003/" "003.eps"
-        "Drawing of a triangle in the poincare plane.\n"
+    vOpts = bestFit 1 defaultView
+  demoImg chan fig EPS vOpts "demo/001/" "001.eps" $
+    "Drawing of a triangle in the cartesian plane.\n"
 
 
-  , let
-      fig :: Rational -> Fig PoincarePlane ()
-      fig t = do
-        p <- coords $ path (CirclePath (0,11/2) 5 0 1) t
-        pen plain p
-        q <- coords (0,3) >>= pen plain
-        segment p q >>= pen plain
-        return ()
+demo002 :: Chan String -> IO ()
+demo002 chan = do
+  let
+    fig :: Rational -> Fig CartesianPlane ()
+    fig t = do
+      p <- coords $ path (CirclePath (0,0) 8 0 1) t
+      pen plain p
+      q <- coords (0,0) >>= pen plain
+      segment p q >>= pen plain
+      return ()
 
-      vOpts = fixed ((-6,-1),(6,12)) defaultView
-      aOpts = frames 50 defaultAnimate
-    in
-      demoAni fig vOpts aOpts "demo/004/" "004"
-        "Animation of a rotating segment in the poincare plane.\n"
+    vOpts = defaultView
+    aOpts = defaultAnimate
+  demoAni chan fig vOpts aOpts "demo/002/" "002" $
+    "Animation of a rotating segment in the cartesian plane.\n"
 
 
-  , let
-      fig :: Rational -> Fig PoincarePlane ()
-      fig t = do
-        p1 <- coords $ path (CirclePath (0,11/2) 5 (0/7) 1) t
-        p2 <- coords $ path (CirclePath (0,11/2) 5 (1/7) 1) t
-        p3 <- coords $ path (CirclePath (0,11/2) 5 (2/7) 1) t
-        p4 <- coords $ path (CirclePath (0,11/2) 5 (3/7) 1) t
-        p5 <- coords $ path (CirclePath (0,11/2) 5 (4/7) 1) t
-        p6 <- coords $ path (CirclePath (0,11/2) 5 (5/7) 1) t
-        p7 <- coords $ path (CirclePath (0,11/2) 5 (6/7) 1) t
-        let ps = [p1,p2,p3,p4,p5,p6,p7]
-        segment p1 p3 >>= pen red
-        segment p2 p4 >>= pen blue
-        segment p3 p5 >>= pen green
-        segment p4 p6 >>= pen red
-        segment p5 p7 >>= pen blue
-        segment p6 p1 >>= pen green
-        segment p7 p2 >>= pen plain
-        sequence $ map (pen plain) ps
-        return ()
+demo003 :: Chan String -> IO ()
+demo003 chan = do
+  let
+    fig :: Fig PoincarePlane ()
+    fig = do
+      p <- coords (1,1) >>= pen plain
+      q <- coords (-2,2) >>= pen plain
+      r <- coords (0,3) >>= pen plain
+      segment p q >>= pen plain
+      segment q r >>= pen plain
+      segment r p >>= pen plain
+      return ()
 
-      vOpts = fixed ((-6,-1),(6,12)) defaultView
-      aOpts = frames 100 defaultAnimate
-    in
-      demoAni fig vOpts aOpts "demo/005/" "005"
-        "Animation of a rotating figure in the poincare plane.\n"
-  ]
+    vOpts = fixed ((-3,-1),(3,4)) defaultView
+  demoImg chan fig EPS vOpts "demo/003/" "003.eps" $
+    "Drawing of a triangle in the poincare plane.\n"
+
+
+demo004 :: Chan String -> IO ()
+demo004 chan = do
+  let
+    fig :: Rational -> Fig PoincarePlane ()
+    fig t = do
+      p <- coords $ path (CirclePath (0,11/2) 5 0 1) t
+      pen plain p
+      q <- coords (0,3) >>= pen plain
+      segment p q >>= pen plain
+      return ()
+
+    vOpts = fixed ((-6,-1),(6,12)) defaultView
+    aOpts = frames 50 defaultAnimate
+  demoAni chan fig vOpts aOpts "demo/004/" "004" $
+    "Animation of a rotating segment in the poincare plane.\n"
+
+
+demo005 :: Chan String -> IO ()
+demo005 chan = do
+  let
+    fig :: Rational -> Fig PoincarePlane ()
+    fig t = do
+      p1 <- coords $ path (CirclePath (0,11/2) 5 (0/7) 1) t
+      p2 <- coords $ path (CirclePath (0,11/2) 5 (1/7) 1) t
+      p3 <- coords $ path (CirclePath (0,11/2) 5 (2/7) 1) t
+      p4 <- coords $ path (CirclePath (0,11/2) 5 (3/7) 1) t
+      p5 <- coords $ path (CirclePath (0,11/2) 5 (4/7) 1) t
+      p6 <- coords $ path (CirclePath (0,11/2) 5 (5/7) 1) t
+      p7 <- coords $ path (CirclePath (0,11/2) 5 (6/7) 1) t
+      let ps = [p1,p2,p3,p4,p5,p6,p7]
+      segment p1 p3 >>= pen red
+      segment p2 p4 >>= pen blue
+      segment p3 p5 >>= pen green
+      segment p4 p6 >>= pen red
+      segment p5 p7 >>= pen blue
+      segment p6 p1 >>= pen green
+      segment p7 p2 >>= pen plain
+      sequence $ map (pen plain) ps
+      return ()
+
+    vOpts = fixed ((-6,-1),(6,12)) defaultView
+    aOpts = frames 100 defaultAnimate
+  demoAni chan fig vOpts aOpts "demo/005/" "005" $
+    "Animation of a rotating figure in the poincare plane.\n"
+
+
+demo006 :: Chan String -> IO ()
+demo006 chan = do
+  let
+    fig :: Rational -> Fig CartesianPlane ()
+    fig t = do
+      a <- coords $ path (CirclePath (0,0) 4    0  1) t
+      o <- coords $ path (CirclePath (0,0) 3 (1/2) 2) t
+      if a == o
+        then do
+          pen plain o
+          return ()
+        else do
+          x <- pointBefore o a
+          c <- circle o a >>= pen plain
+          let Just b = cutCircleRay (o,a) x
+          r <- ray o x >>= pen plain
+          pen plain x
+          pen red o
+          pen blue a
+          pen green b
+          return ()
+
+    vOpts = fixed ((-20,-20),(20,20)) defaultView
+    aOpts = frames 100 defaultAnimate
+  demoAni chan fig vOpts aOpts "demo/006/" "006" $
+    "Construction of an antipode in the cartesian plane.\n" ++
+    "(Antipode of blue through red at green.)\n"
+
